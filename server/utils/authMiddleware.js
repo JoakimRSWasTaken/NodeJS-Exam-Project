e074d4ab92e30486b1ca6e9bc350e89795b000fc
Joakim Rosdahl Severinsen <jose0001@stud.ek.dk>
2026-05-17 13:23:03 +0200

    authMiddleware.js er ændret så den tager et array a roles og tjekker om brugerens rolle er i den. Det kunne lige så godt bare være en enkelt rolle, men det er en smule mere skalerbart sådan her.

--- server/utils/authMiddleware.js
+++ server/utils/authMiddleware.js
@@ -1,4 +1,4 @@
-export async function isLoggedIn (req, res, next) {
+export async function isLoggedIn(req, res, next) {
     if (req.session.user) {
         return next();
     } else {
@@ -6,12 +6,17 @@ export async function isLoggedIn (req, res, next) {
     }
 }
 
-export async function isAdmin(req, res, next) {
-    // Checker om user er logget ind som admin
-    // Hvis ikke, får de en fejl
-    if (req.session.user.isAdmin === 1) {
-        return next();
-    } else {
-        return res.status(403).send({ errorMessage: "You don't have the right, O, you don't have the right." });
+export async function isAllowedRole(allowedRoles) {
+    return (req, res, next) => {
+
+        const role = req.session.user.role;
+
+        if (role === 'admin') { // Checker om user er logget ind som admin først
+            return next();
+        } else if (allowedRoles.includes(role)) { // Checker så user er logget ind som den ellers forventede rolle
+            return next();
+        } else {
+            return res.status(403).send({ errorMessage: "You don't have the right, O, you don't have the right." });
+        }
     }
 }
